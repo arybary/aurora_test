c49888523c6a25a3f4b351dda12e72cec4b2468e
arybary <l3fljl5ldydy@gmail.com>
2022-09-03 21:20:39 +0300

    menu-modal+

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-const handleClick = event => {
-    const lists = document.querySelectorAll(".navigation__list");
-    const icons = document.querySelectorAll(".fa-chevron-right")
-    console.log(lists);
-    const targetList = event.target.parentElement.nextElementSibling;
-    const targetIcon = event.target;
-    console.log(targetList, targetIcon );
-    
-  
-    lists.forEach(item => {
-      if (item === targetList) {
-        item.classList.toggle("show")
-      }
-    });
-    icons.forEach(item => {
-        if (item === targetIcon) {         
-          item.classList.toggle("rotate")
-        }
-      })
+const handleClick = (event) => {
+  const lists = document.querySelectorAll(".navigation__list");
+  const icons = document.querySelectorAll(".fa-chevron-right");
+  const targetList = event.target.parentElement.nextElementSibling;
+  const targetIcon = event.target;
+  lists.forEach((item) => {
+    if (item === targetList) {
+      item.classList.toggle("show");
+    }
+  });
+  icons.forEach((item) => {
+    if (item === targetIcon) {
+      item.classList.toggle("rotate");
+    }
+  });
+};
 
-  }
-  
-  document.querySelectorAll(".navigation__title").forEach(item => {
-    item.addEventListener("click", handleClick)
-  })
+document.querySelectorAll(".navigation__title").forEach((item) => {
+  item.addEventListener("click", handleClick);
+});
+
+const menu = document.querySelector(".fa-bars");
+
+const menuOpen=()=>{   
+    const modalMenu = document.querySelector(".menu");
+    modalMenu.classList.add("menu-show");
+}
+
+menu.addEventListener("click", menuOpen);
+
+const closeMenu = document.querySelector(".close");
+
+const menuClose=()=>{   
+    const modalMenu = document.querySelector(".menu");
+    modalMenu.classList.remove("menu-show");
+    console.log('huy')
+    
+}
+
+closeMenu.addEventListener("click", menuClose);
