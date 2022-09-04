2bb037490bc6e5b5030af7db573c331bf90f94be
arybary <l3fljl5ldydy@gmail.com>
2022-09-04 18:56:42 +0300

    search button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,73 @@
-const handleClick = (event) => {
+/** @format */
+
+const menuBtn = document.querySelector(".fa-bars");
+
+const menuOpen = () => {
+  const modalMenu = document.querySelector(".menu");
+  modalMenu.classList.add("menu-show");
+};
+
+menuBtn.addEventListener("click", menuOpen);
+
+const closeMenuBtn = document.querySelector(".menu-close");
+
+const menuClose = () => {
+  const modalMenu = document.querySelector(".menu");
+  modalMenu.classList.remove("menu-show");
+};
+
+closeMenuBtn.addEventListener("click", menuClose);
+
+const menuItemBtn = document.querySelectorAll(".menu-navigation__item");
+
+const openNextMenu = (event) => {
+  const prevMenu = event.target.parentElement;
+
+  const nextMenu = document.querySelector(`#next-${event.target.id}`);
+  if (nextMenu !== null) {
+    prevMenu.hidden = true;
+    nextMenu.hidden = false;
+  } else {
+    alert(`Not maket ${event.target.id}`);
+  }
+};
+
+menuItemBtn.forEach((item) => {
+  item.addEventListener("click", openNextMenu);
+});
+
+const prevMenuBtn = document.querySelectorAll(".menu-next__title");
+
+const openPrevMenu = (event) => {
+  const nowMenu = event.target.parentElement.parentElement.parentElement;
+  const menu = document.querySelector(`#menu`);
+  menu.hidden = false;
+  nowMenu.hidden = true;
+};
+
+prevMenuBtn.forEach((item) => {
+  item.addEventListener("click", openPrevMenu);
+});
+
+const searchBtn = document.querySelector("#search");
+
+const openSearch = (event) => {
+  const icon = document.querySelector(".fa-magnifying-glass");
+ icon.classList.toggle("fa-xmark");
+
+
+  const searchInput = document.querySelector(".search");
+  console.log(icon);
+  searchInput.classList.toggle("search-show");
+};
+
+searchBtn.addEventListener("click", openSearch);
+
+const footerNavBtn = document.querySelectorAll(".navigation__title");
+
+const navItemClick = (event) => {
   const lists = document.querySelectorAll(".navigation__list");
-  const icons = document.querySelectorAll(".fa-chevron-right");
+  const icons = document.querySelectorAll(".navigation__icon");
   const targetList = event.target.parentElement.nextElementSibling;
   const targetIcon = event.target;
   lists.forEach((item) => {
@@ -15,26 +82,6 @@ const handleClick = (event) => {
   });
 };
 
-document.querySelectorAll(".navigation__title").forEach((item) => {
-  item.addEventListener("click", handleClick);
+footerNavBtn.forEach((item) => {
+  item.addEventListener("click", navItemClick);
 });
-
-const menu = document.querySelector(".fa-bars");
-
-const menuOpen=()=>{   
-    const modalMenu = document.querySelector(".menu");
-    modalMenu.classList.add("menu-show");
-}
-
-menu.addEventListener("click", menuOpen);
-
-const closeMenu = document.querySelector(".close");
-
-const menuClose=()=>{   
-    const modalMenu = document.querySelector(".menu");
-    modalMenu.classList.remove("menu-show");
-    console.log('huy')
-    
-}
-
-closeMenu.addEventListener("click", menuClose);
